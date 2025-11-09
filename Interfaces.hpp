ba07aef1218f3039cf1193c36e72b848a42377e1
acheng2007 <163091516+acheng2007@users.noreply.github.com>
2025-11-09 00:47:07 -0500

    interfaces

--- Interfaces.hpp
+++ Interfaces.hpp
@@ -6,10 +6,11 @@
 
 template <typename T>
 class StackInterface {
+    public:
     virtual void push(const T& data)= 0;
-    virtual void pop() = 0;
+    virtual T pop() = 0;
     virtual T peek() const = 0;
-    virtual size_t getSize() const noexcept = 0;
+    virtual std::size_t getSize() const noexcept = 0;
 
     virtual ~StackInterface() = default;
 };
@@ -17,10 +18,11 @@ class StackInterface {
 
 template <typename T>
 class QueueInterface {
-    void enqueue(const T& item)= 0;
-    T dequeue() = 0;
-    T peek() const = 0;
-    size_t getSize() const noexcept = 0;
+    public:
+    virtual void enqueue(const T& item)= 0;
+    virtual T dequeue() = 0;
+    virtual T peek() const = 0;
+    virtual std::size_t getSize() const noexcept = 0;
 
     virtual ~QueueInterface() = default;
 };
@@ -28,13 +30,14 @@ class QueueInterface {
 
 template <typename T>
 class DequeInterface {
-    void pushFront(const T& item) = 0;
-    void pushBack(const T& item) = 0;
-    T popFront() = 0;
-    T popBack() = 0;
-    const T& front() const = 0;
-    const T& back() const = 0;
-    size_t getSize() const noexcept = 0;
+    public:
+    virtual void pushFront(const T& item) = 0;
+    virtual void pushBack(const T& item) = 0;
+    virtual T popFront() = 0;
+    virtual T popBack() = 0;
+    virtual const T& front() const = 0;
+    virtual const T& back() const = 0;
+    virtual std::size_t getSize() const noexcept = 0;
 
     virtual ~DequeInterface() = default;
 };
