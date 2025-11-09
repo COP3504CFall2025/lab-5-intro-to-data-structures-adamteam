aad051cf585e617c1dcbf5cfa4ef61574649e038
acheng2007 <163091516+acheng2007@users.noreply.github.com>
2025-11-09 03:10:37 -0500

    t pusdsd

--- LLDQ.hpp
+++ LLDQ.hpp
@@ -15,22 +15,47 @@ private:
 
 public:
     // Constructor
-    LLDQ();
+    LLDQ() = default;
 
     // Core Insertion Operations
-    void pushFront(const T& item) override;
-    void pushBack(const T& item) override;
+    void pushFront(const T& item) override{
+        list.addHead(item);
+    }
+    void pushBack(const T& item) override{
+        list.addTail(item);
+    }
 
     // Core Removal Operations
-    T popFront() override;
-    T popBack() override;
+    T popFront() override{
+        if(list.getCount() == 0) throw std::runtime_error("empty");
+
+        T data = list.getHead()->data;
+        list.removeHead();
+        return data;
+    }
+    T popBack() override{
+        if(list.getCount() == 0) throw std::runtime_error("empty");
+
+        T data = list.getTail()->data;
+        list.removeTail();
+        return data;
+    }
 
     // Element Accessors
-    const T& front() const override;
-    const T& back() const override;
+    const T& front() const override{
+        if(list.getCount() == 0) throw std::runtime_error("empty");
+
+        return list.getHead()->data;
+    }
+    const T& back() const override{
+        if(list.getCount() == 0) throw std::runtime_error("empty");
+        return list.getTail()->data;
+    }
 
     // Getter
-    std::size_t getSize() const noexcept override;
+    std::size_t getSize() const noexcept override{
+        return list.getCount();
+    }
 };
 
 
